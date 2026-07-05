db6b10357074968c03f21a6eb21edc581205880d
LemonadeN126 <tipsysummeraa@gmail.com>
2026-07-06 04:47:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-console.log("JS CLEAN VERSION");
+console.log("JS IS RUNNING");
 
+const loading = document.getElementById("loadingText");
 const btn = document.getElementById("enterButton");
 
+const text = [
+    "Loading.",
+    "Loading..",
+    "Loading...",
+    "Connection established."
+];
+
+let i = 0;
+
+const timer = setInterval(() => {
+
+    if (loading) loading.textContent = text[i];
+
+    if (i === text.length - 1) {
+        clearInterval(timer);
+        btn.classList.add("show");
+    }
+
+    i++;
+
+}, 700);
+
+
 btn.addEventListener("click", () => {
-    alert("ENTER WORKS");
+    alert("ENTER CLICKED");
 
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "block";
